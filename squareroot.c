6838d4923cccde5eb007f96fcce39b9763085ab0
Shohibul <shohibul602@gmail.com>
2025-08-09 23:13:14 +0600

    added C program

---
+++ squareroot.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+int squareroot(int n);
+
+int main(){
+    int n;
+    printf("Enter your value:");
+    scanf("%d",&n);
+    squareroot(n);
+    return 0;
+}
+
+int squareroot(int n){
+    int i;
+    int half=n/2;
+    for(i=1; i<=half; i++){
+        if(i*i == n){
+            printf("Square root is: %d\n",i);
+            break;
+        }
+    }
+    if(i>n){
+        printf("Its not a perfect square!");
+    }
+}
